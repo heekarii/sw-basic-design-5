b9746d5a29ec926482babffe9c91c4b6fea69d5d
heekarii <hantecude123@gmail.com>
2025-11-16 17:15:23 +0900

    Feat: Add battery Status UI

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
 
 public class GameManager : Singleton<GameManager>
 {
     [Header("Game Status")] 
     [SerializeField] private int _curScrap;
     [SerializeField] private float _curBattery;
+    private PlayerStatus _playerStatus;
+
+    [Header("UI Elements")] 
+    [SerializeField] private Image _batteryFillbar;
+    [SerializeField] private TextMeshProUGUI _batteryText;
+    
+    
+    
     public Player Player;
     public int WeaponType; // 0 : Short, 1 : long
-    
-    // [Header("UI")]
     
     
     protected override void Awake()
@@ -34,10 +43,23 @@ public class GameManager : Singleton<GameManager>
         
     }
 
+    private void UpdateStatusUI()
+    {
+        _batteryFillbar.fillAmount = _curBattery / 100f;
+        _batteryText.text = $"{_curBattery:F2}%";
+    }
+    
+    private void Update()
+    {
+        _playerStatus = Player.GetStatus();
+        _curBattery = _playerStatus.BatteryRemaining;
+        UpdateStatusUI();
+        
+    }
+    
     public void AddScrap(int amount)
     {
         _curScrap += amount;
         Debug.Log($"[GameManager] 스크랩 {_curScrap} 보유 중");
     }
-    
 }
